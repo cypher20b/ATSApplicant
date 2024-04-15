d6f676acfb62049716fe1d5c1a5841f0c4d1bd73
cypher20b <nuel.emma20@gmail.com>
2024-04-16 00:47:38 +0100

    feat: added a new route to point to the jobs based on geographical area

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { JobsOnMapComponent } from './jobs-on-map/jobs-on-map.component';
 
 export const routes: Routes = [
-    {path:'', component:HomeComponent}
+    {path:'', component:HomeComponent},
+    {path:'/jobs-on-map', component:JobsOnMapComponent}
 ];
