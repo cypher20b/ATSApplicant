f74ae4436ddd977fc764d0c3baf3929f51243237
cypher20b <nuel.emma20@gmail.com>
2024-04-24 03:23:11 +0100

    feat(route): added new route 'apply' to enable navigation to the job application page.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { JobsOnMapComponent } from './jobs-on-map/jobs-on-map.component';
+import { ApplicationPageComponent } from './components/application-page/application-page.component';
 
 export const routes: Routes = [
     {path:'', component:HomeComponent},
+    {path:'apply', component:ApplicationPageComponent},
     // {path:'/jobs-on-map', component:JobsOnMapComponent}
 ];
