78720c8840f6e009347dcb676a31d8ae2d99e369
cypher20b <nuel.emma20@gmail.com>
2024-04-26 18:41:32 +0100

    feat(modified): added new api call to get data from backend server, and added an import for the environment variables

--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ResCountryResponse } from '../interfaces/res-country-response';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment.development';
+import { JobApplicaionData } from '../interfaces/job-applicaion-data';
 @Injectable({
   providedIn: 'root'
 })
@@ -9,6 +11,10 @@ export class ApiService {
 
   constructor(private http: HttpClient) { }
   getAllCountryCodes() {
-    return this.http.get<ResCountryResponse[]>('https://restcountries.com/v3.1/all?fields=name,idd,flags');
+    return this.http.get<ResCountryResponse[]>(`${environment.areaCodeApi}/all?fields=name,idd,flags`);
+  }
+
+  applyToJob(data:JobApplicaionData): Observable<ResCountryResponse>{
+    return this.http.post<ResCountryResponse>(`${environment.baseUrl1}/apply`, data)
   }
 }
